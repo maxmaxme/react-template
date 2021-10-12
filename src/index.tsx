547aa09a787665b9ea72a9167310f34ec6a1d3c8
Maxim Lepekha <m.lepekha@yandex.ru>
2021-10-12 23:11:38 +0300

    fixes

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,11 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { App } from './components/App';
+import './index.css';
 
-render(<App />, document.getElementById('root'));
+render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root'),
+);
